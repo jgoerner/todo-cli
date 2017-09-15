f522bd32b00a72a704fd340c644316e0c027f1a5
jgoerner <joshua.goerner@gmail.com>
2017-09-16 00:56:36 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup
 setup(
     name="todo-cli",
     version="0.1",
+    author="Joshua Goerner",
+    author_email="joshua.goerner@gmail.com",
     py_modules=["todo_cli"],
     install_requires=[
         "Click",
